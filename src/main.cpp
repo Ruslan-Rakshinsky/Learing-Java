9db016ba97ac88f2aa7b3b55d91a3f854e0e5fae
Ruslan Rakshinsky <ruslanfox.eerror@outlook.com>
2023-01-09 22:48:28 +0300

    added return statement

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <exception>
 #include <iostream>
 #include "Window.h"
@@ -5,7 +6,9 @@
 int main(int, char **) {
     try {
         Window* window = new Window(800, 600, "Test-1");
+        return EXIT_SUCCESS;
     } catch (std::exception const & e) {
         std::cerr << e.what();
+        return EXIT_FAILURE;
     }
 }
